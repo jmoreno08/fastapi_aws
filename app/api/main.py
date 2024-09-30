c16eb83e94cafc38efca67b322e4fb1c3c67a4cc
Jonathan Moreno <morenoelectro@gmail.com>
2024-09-30 06:49:43 -0500

    refactor: :recycle: Update main.py: Configurar CORS middleware

--- app/api/main.py
+++ app/api/main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI
 from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
+
+# Configurar CORS
+origins = [
+    "http://localhost:5173",
+    "https://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*", "access-control-allow-methods", "access-control-allow-origin", "authorization", "content-type"],
+)
 
 @app.get("/")
 async def root():
     return{"message":"Hola el mo"}
 
-@app.get("/docs")
-def read_docs():
-    return {"docs": "Este es el punto de documentación de FastAPI."}
 
 handler = Mangum(app=app)
